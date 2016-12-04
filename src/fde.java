24aad59c2e8b1f52f95eb69bf0bff83c6c943615
Juan <pizajoan97@gmail.com>
2016-12-04 18:15:06 +0100

    Afegim tots els arxius

---
+++ src/fde.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Juan on 24/11/2016.
+ */
+public class fde {
+    public static void main(String[] args) {
+        int ar[][] = new int[2][2];
+        ar[0][0] = 1;
+        ar[0][1] = 3;
+        ar[1][0] = 2;
+        ar[1][1] = 4;
+        System.out.println(ar[0][0] * ar[1][1] - ar[0][1] * ar[1][0]);
+
+
+    }
+}
